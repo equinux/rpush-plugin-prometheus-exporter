d13b6d7944be9995fe525366b1ed4141e1f40276
Maximilian Szengel <szengel@equinux.com>
2021-06-17 18:02:45 +0200

    Prepare for in-flight notifications gauge

--- lib/rpush/plugin/prometheus_exporter.rb
+++ lib/rpush/plugin/prometheus_exporter.rb
@@ -10,14 +10,23 @@ plugin.configure do |config|
   config.port = 9394
 end
 
-REFLECTIONS = [:error, :apns_feedback, :notification_enqueued, :notification_delivered,
-               :notification_failed, :notification_id_failed, :notification_will_retry,
-               :notification_id_will_retry, :tcp_connection_lost, :gcm_delivered_to_recipient,
-               :gcm_failed_to_recipient, :gcm_invalid_registration_id, :adm_failed_to_recipient]
+REFLECTIONS = [:error, :apns_feedback, :notification_enqueued,
+               :notification_delivered, :notification_failed,
+               :notification_id_failed, :notification_will_retry,
+               :notification_id_will_retry, :tcp_connection_lost,
+               :gcm_delivered_to_recipient, :gcm_failed_to_recipient,
+               :gcm_invalid_registration_id, :adm_failed_to_recipient]
 
 plugin.reflect do |on|
   REFLECTIONS.each do |reflection|
-    block = ->(*args) { @counter.observe(1, event: reflection.to_s) }
+    block = ->(*args) do
+      if reflection == :notification_enqueued
+        @enqueued_notifications_gauge.increment()
+      elsif [:notification_delivered, :notification_failed].include?(reflection)
+        @enqueued_notifications_gauge.decrement()
+      end
+      @counter.observe(1, event: reflection.to_s)
+    end
     on.send(reflection, &block)
   end
 end
@@ -28,5 +37,14 @@ plugin.init do
     host: plugin.config.host,
     port: plugin.config.port,
   )
-  @counter = @client.register(:counter, "rpush", "Rpush event counter")
+  @counter = @client.register(
+    :counter,
+    "rpush_events_total",
+    "Rpush event counter"
+  )
+  @enqueued_notifications_gauge = @client.register(
+    :gauge,
+    "rpush_enqueued_notifications",
+    "Rpush currently enqueued notifications"
+  )
 end
